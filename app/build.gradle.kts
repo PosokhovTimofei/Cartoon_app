c931aaacae92f1172901c100b91123e93fd6372f
Posokhov Timofei <new-ur@list.ru>
2024-06-02 23:51:27 +0300

    Добвылние регистрации

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,22 +27,29 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("com.google.firebase:firebase-analytics")
+
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
-    implementation(platform("com.google.firebase:firebase-auth:23.0.0"))
-    implementation(platform("com.google.firebase:firebase-database:21.0.0"))
-    implementation("com.google.android.material:material:1.12.0")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    // Picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
 
 
-
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/cartoon/MainActivity.java
+++ app/src/main/java/com/example/cartoon/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.cartoon;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_window);
+        setContentView(R.layout.activity_main);
+
+
     }
 }
